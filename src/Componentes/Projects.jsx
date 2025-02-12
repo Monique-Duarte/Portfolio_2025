afbb709558fff4e03ca631faf1b51ec539c7a6eb
Monique Duarte <moniquead95@gmail.com>
2025-02-12 12:47:48 -0300

    update img

--- src/Componentes/Projects.jsx
+++ src/Componentes/Projects.jsx
@@ -27,6 +27,14 @@ const projetosData = [
     githubLink: "https://github.com/Monique-Duarte/Projeto-3-Page-Restaurante.git",
     },
     {
+    title: "Portifólio Less",
+    imgSrc: "/img/portless.png",
+    alt: "Miniatura do site",
+    tags: "#JS #CSS #HTML #Jquery #Less",
+    siteLink: "https://portfolio-less-seven.vercel.app/",
+    githubLink: "https://github.com/Monique-Duarte/Portfolio-Less.git",
+    },
+    {
     title: "Blog de Noticias",
     imgSrc: "/img/image.png",
     alt: "Jornal",
@@ -49,14 +57,6 @@ const projetosData = [
     tags: "#JS #CSS #HTML #Bootstrap",
     siteLink: "https://site-game-shop-ten-coral.vercel.app/",
     githubLink: "https://github.com/Monique-Duarte/Site-Game-Shop.git",
-    },
-    {
-    title: "Portifólio Less",
-    imgSrc: "/img/portless.png",
-    alt: "Miniatura do site",
-    tags: "#JS #CSS #HTML #Jquery #Less",
-    siteLink: "https://portfolio-less-seven.vercel.app/",
-    githubLink: "https://github.com/Monique-Duarte/Portfolio-Less.git",
     },
     {
     title: "Portal de Notícias",
