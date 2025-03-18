46f32f0a24a6c079634ff07641725be81ec30ce8
Monique Duarte <moniquead95@gmail.com>
2025-03-18 08:55:07 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import Header from "./Componentes/Header/Header";
 import Projects from "./Componentes/Projects/Projects";
 import Footer from "./Componentes/Footer/Footer";
 import Menu from "./Componentes/Menu/Menu";
+import "./App.css"
 
 function App() {
   return (
     <>
       <main className="main">
+        <div style={{ backgroundColor: "#000916" }}>
+          <Menu />
+        </div>
         <Header />
         <Projects />
       </main>
-      <div style={{ backgroundColor: "#000916"}}>
-        <Menu />
-      </div>
       <Footer />
     </>
   );
