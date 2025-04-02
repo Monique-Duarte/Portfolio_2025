08d4a7d09e174e0e0ab03b50893637fa190636e6
Monique Duarte <moniquead95@gmail.com>
2025-04-02 13:50:21 -0300

    container

--- src/Componentes/Sobre/index.jsx
+++ src/Componentes/Sobre/index.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 const SobreMim = () => {
   const { t } = useTranslation(); // Adicione esta linha para obter a função t
   return (
-    <div id="SobreMim" className="flex mx-auto justify-center text-justify py-12 shadow-[inset_0_0_0_3px_#002D49] px-4 max-w-[80vw] font-winky">
+    <div id="SobreMim" className="flex mx-auto justify-center text-balance py-12 shadow-[inset_0_0_0_3px_#002D49] px-4 max-w-[80vw] font-winky">
       <div className="flex flex-col items-center justify-center w-full sm:max-w-[80%] lg:max-w-[70%] text-center text-base sm:text-xl">
         <h1 className="text-2xl mb-5 relative overflow-y-hidden">Sobre Mim
           <span className="absolute bottom-0 left-0 w-0 h-[4px] bg-[#0074ae]"></span>
