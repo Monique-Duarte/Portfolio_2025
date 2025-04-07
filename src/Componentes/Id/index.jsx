66df3507c9f0147de3ff6779da191991bebf638f
Monique Duarte <moniquead95@gmail.com>
2025-04-07 10:07:57 -0300

    id

--- src/Componentes/Id/index.jsx
+++ src/Componentes/Id/index.jsx
@@ -6,19 +6,20 @@ import Contact from '../Contact';
 const Id = () => {
   const { t } = useTranslation(); // Adicione esta linha para obter a função t
   return (
-    <div id='id' className="bg-[#8F87F1]">
-      <div className="md:h-[70vh] w-[80vw] mx-auto block center text-balance font-roboto md:flex bg-[#8F87F1]">
-        <div id="imgFundo" className="items-center justify-center mx-25 md:my-20 h-auto relative md:w-[50vw]">
+    <div className="bg-[#8F87F1]">
+      <div id='id' className="bg-[#8F87F1] mx-2 md:mx-20 md:h-[70vh] block center text-balance md:flex">
+        <div id="imgFundo" className="items-center relative md:w-[50vw] md:my-auto">
           <img
-            className="md:w-[400px] shadow-none" 
+            className="md:my-auto md:mx-auto md:w-1/2 w-60 mx-auto shadow-none"
             src="/img/monique1.png"
             alt="foto perfil"
           />
         </div>
-        <div className='text-center md:text-center md:my-auto md:w-[50vw]'>
+        <div className='text-center w-[100vw] md:my-auto md:w-[60vw]'>
           <h1 className="text-lg my-2 overflow-hidden">Monique Duarte</h1>
           <p className="text-balance mx-10 text-lg mb-1 md:text-2xl">{t('id.graduacao')}</p>
           <p className="text-lg  mx-10 mb-1 md:text-2xl ">{t('id.area')}</p>
+          <p className="text-lg  mx-10 mb-1 md:text-2xl ">{t('id.Estudante')}</p>
           <p className="text-lg mx-10 mb-1 md:text-l "> {t('id.tags')} </p>
           <div className='flex my-0 items-center justify-center md:flex'><Contact /> </div>
         </div>
